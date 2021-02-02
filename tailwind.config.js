33f72a303bc2e54869d01f0523f6a1c59ff667f6
Lucas <lucas98fernando@gmail.com>
2021-02-01 23:52:30 -0300

    Updated files

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  prefix: 'tw-',
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
