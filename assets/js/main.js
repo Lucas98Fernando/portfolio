4d501adad1743f2c90535dd03ea3449cc548a8b5
Lucas <lucas98fernando@gmail.com>
2021-02-03 23:39:10 -0300

    Adding menu and animations

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+	var navicon = document.getElementById("nav-icon")
+	$('#nav-icon').click(function () {
+		$(this).toggleClass('open');
+
+		if (navicon.style.position === "fixed") {
+			navicon.style.position = "relative";
+		}
+		else {
+			navicon.style.position = "fixed";
+		}
+	});
+});
+
+function mobileToggle() {
+	var x = document.getElementById("navbar-side");
+	if (x.style.display === "flex") {
+		x.style.display = "none";
+	} else {
+		x.style.display = "flex";
+	}
+}
